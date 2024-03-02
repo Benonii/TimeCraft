d51374101f1c034ec3736ea122ed46a7ed1da3fe
BENONI ESCKINDER <you@example.com>
2024-03-02 14:42:36 +0300

    Added tests for the models, next is the engine

---
+++ tests/test_models/test_task.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module contains tests for task.py """
+
+import unittest
+import sqlalchemy
+from models.task import Task
+
+
+class TestTask(unittest.TestCase):
+    """ Tests the Task class """
+
+    def test_task_attrs(self):
+        """ Tests if the task attributes work correctly """
+        task = Task()
+        task.task_name = "Study"
+        task.daily_goal = 6
+        task.weekly_goal = 36
+        task.user_id = "this is the user id"
+
+        self.assertEqual(type(task), Task)
+        self.assertEqual(task.task_name, "Study")
+        self.assertEqual(task.daily_goal, 6)
+        self.assertEqual(task.weekly_goal, 36)
+        self.assertEqual(task.user_id, "this is the user id")
+        self.assertEqual(type(task.logs),
+                         sqlalchemy.orm.collections.InstrumentedList)
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This module contains unittest for user.py """
+
+import unittest
+from models.user import User
+from models import storage
+
+
+class TestUser(unittest.TestCase):
+    """ Tests the user class """
+
+    def setUp(self):
+        """ I do need this after all """
+        self.user = User()
+        self.user.name = "John"
+        self.user.weekly_work_hours_goal = 60
+        self.user.number_of_work_days = 6
+        self.user.total_productive_time = 0
+        self.user.total_wasted_time = 0
+
+    def test_user_attributes(self):
+        """ Tests the type and value of a user's attributes """
+        self.assertEqual(type(self.user), User)
+        self.assertEqual(self.user.name, "John")
+        self.assertEqual(self.user.weekly_work_hours_goal, 60)
+        self.assertEqual(self.user.number_of_work_days, 6)
+        self.assertEqual(self.user.total_productive_time, 0)
+        self.assertEqual(self.user.total_wasted_time, 0)
