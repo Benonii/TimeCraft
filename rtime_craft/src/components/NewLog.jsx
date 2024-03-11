b395ed0d0bbeb32578751562785676e03d8fe28f
BENONI ESCKINDER <you@example.com>
2024-03-11 23:59:56 +0300

    Functionality for new Task and new Log

--- rtime_craft/src/components/NewLog.jsx
+++ rtime_craft/src/components/NewLog.jsx
@@ -1,26 +1,72 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function NewLog() {
+	const [formData, setFormData] = useState({
+		taskId: '',
+		timeOnTask: '',
+		timeWasted: ''
+	});
+
+	function handleChange(e) {
+	    const { name, value } = e.target;
+	    setFormData(prevState => ({
+		    ...prevState,
+		    [name]: value
+	}));
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			const params = new URLSearchParams();
+			params.append('taskId', formData.taskId)
+			params.append('timeOnTask', formData.timeOnTask)
+			params.append('timeWasted', formData.timeWasted)
+
+			const response = await fetch ("http://127.0.0.1:5001/tc/v1/new_log", {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				body: params.toString(),
+				mode: 'no-cors'
+				});
+
+				if (response !== {}) {
+					console.log("Form submitted successfully")
+				} else {
+					console.error("Failed to submit form")
+				};
+		} catch(e) {
+			console.error("Error submitting form:", e);
+		}
+	};
+
 	return (
 	    <main className="new-log-container">
 		<h1 className="title">New Log</h1>
-    		<label htmlfor="task-id">What is the Task ID?</label>
-    		<br />
-    		<input type="text" name="task-id" />
+		<form onSubmit={handleSubmit}>
+ 	   		<label htmlFor="taskId">What is the Task ID?</label>
+    			<br />
+    			<input type="text" name="taskId" onChange={handleChange}/>
 
-    		<br /><br />
+	    		<br /><br />
 
-    		<label htmlfor="tot">How much time did you spend on
-		    web-designing today</label>
-    		<br />
-    		<input type="text" name="tot" />
+	    		<label htmlFor="timeOnTask">How much time did you spend on
+			    web-designing today</label>
+    			<br />
+    			<input type="text" name="timeOnTask" onChange={handleChange} />
 
-    		<br /><br />
+ 	   		<br /><br />
 
-    		<label htmlfor="tw">How much time have you wasted today?<br />
-                    (It's okay, I won't judge)</label>
-    		<br />
-    		<input type="text" name="tw" />
+	    		<label htmlFor="timeWasted">How much time have you wasted today?<br />
+        	            (It's okay, I won't judge)</label>
+    			<br />
+    			<input type="text" name="timeWasted" onChange={handleChange} />
+		</form>
+
+		<button type="submit" onClick={handleSubmit}>Log</button>
 	    </main>
 	)
 }
