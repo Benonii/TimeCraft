0fc8006cf74f173bf19bd29551f8b69511c67d2e
BENONI ESCKINDER <besckinder@gmail.com>
2024-03-18 17:48:03 +0300

    Making final additions

--- rtime_craft/src/App.jsx
+++ rtime_craft/src/App.jsx
@@ -5,13 +5,70 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 import "./style.css"
 
+
 export default function App() {
 	const [action, setAction] = React.useState(0)
-
+	const [userId, setUserId] = React.useState("");
+	
 	function onActionClick(id) {
 		setAction(id)
 	}
-    return (
+
+	async function getUser() {
+		try {
+
+			const response = fetch("http://localhost:5001/tc/v1/get_session_user",
+						{
+							method: 'GET',
+							headers: {
+								'Content-Type': 'application/json'
+							}
+						});
+			if (response.ok) {
+				const responseJSON = await response.json();
+				const userId = responseJSON.user_id;
+				
+				if (userId !== null || userId !== "") {
+					setUserId(userId);
+					console.log("Ladies and Gentlemen, we got him");
+				} else {
+					console.error("User ID is null, just thought you should know");
+				};
+			} else {
+				console.error("Couldn't get user");
+				console.log(response)
+			};
+		} catch(error){
+			console.error("Error submitting form: ", error);
+		};
+	};
+	getUser();
+
+	async function assignUser(userID) {
+		
+		if (userID === null || userID === "") {
+			console.error("Please enter a valid User ID")
+		} else { 
+			const param = new URLSearchParams();
+			param.append('userID', userID)
+			const response = fetch("http://localhost:5001/tc/v1/switch_user",
+						{
+							method: 'POST',
+							headers: {
+								'Content-Type': 'x-www-form-urlencoded'
+							},
+							body: param.toString(),
+						})
+			if (response.ok) {
+				setUserId(userId);
+				console.log("User Changed Successfully");
+			} else {
+				console.error("Couldn't switch user");
+			}
+		};
+	};	
+
+	return (
         <main>
 	    <Split
 		sizes={[30, 70]}
@@ -21,7 +78,7 @@ export default function App() {
 		<Sidebar
 	    	    onClick={onActionClick}
 		/>
-                <Main actionId={action}/>
+                <Main actionId={action} userId={userId} />
               
             </Split>
         </main>
